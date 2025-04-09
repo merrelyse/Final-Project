63811cc6269759dae9aee9c1ad9d0b16ee9555d8
Merritt Leidy <merritt.leidy@gmail.com>
2025-04-09 13:05:10 -0400

    yeah

--- audiostartstop.js
+++ audiostartstop.js
@@ -2,26 +2,32 @@
 
 const soundCtx = new AudioContext();
 
-const gainGate = soundCtx.createGain();
-gainGate.gain.value = 1.0;
+let audioBufferMM;
+let audioBufferB;
+let audioBufferOMW;
+let audioBufferE;
 
 let sourceMM;
 let analyserMM;
+let gainGateMM;
 const loadPlayAudioMM = async function () {
   const file1 = await fetch("Memory Machine.wav");
   const arrayBufferMM = await file1.arrayBuffer();
-  const audioBufferMM = await soundCtx.decodeAudioData(arrayBufferMM);
+  audioBufferMM = await soundCtx.decodeAudioData(arrayBufferMM);
   sourceMM = soundCtx.createBufferSource();
   sourceMM.buffer = audioBufferMM;
+
+  gainGateMM = soundCtx.createGain();
+  gainGateMM.gain.value = 1.0;
 
   analyserMM = soundCtx.createAnalyser();
   analyserMM.fftSize = 2048;
   const bufferLengthMM = analyserMM.frequencyBinCount;
   const dataArrayMM = new Uint8Array(bufferLengthMM);
 
-  sourceMM.connect(gainGate);
-  gainGate.connect(analyserMM);
-  gainGate.connect(soundCtx.destination);
+  sourceMM.connect(gainGateMM);
+  gainGateMM.connect(analyserMM);
+  gainGateMM.connect(soundCtx.destination);
 
   const updateAnalyserMM = function () {
     analyserMM.getByteFrequencyData(dataArrayMM);
@@ -33,26 +39,30 @@ const loadPlayAudioMM = async function () {
 
 let sourceB;
 let analyserB;
+let gainGateB;
 const loadPlayAudioB = async function () {
   const file2 = await fetch("Being.wav");
   const arrayBufferB = await file2.arrayBuffer();
-  const audioBufferB = await soundCtx.decodeAudioData(arrayBufferB);
+  audioBufferB = await soundCtx.decodeAudioData(arrayBufferB);
   sourceB = soundCtx.createBufferSource();
   sourceB.buffer = audioBufferB;
+
+  gainGateB = soundCtx.createGain();
+  gainGateB.gain.value = 1.0;
 
   analyserB = soundCtx.createAnalyser();
   analyserB.fftSize = 512;
   const bufferLengthB = analyserB.frequencyBinCount;
   const dataArrayB = new Uint8Array(bufferLengthB);
-  sourceB.connect(gainGate);
-  gainGate.connect(analyserB);
-  gainGate.connect(soundCtx.destination);
 
-  const detectBeats = function () {
+  sourceB.connect(gainGateB);
+  gainGateB.connect(analyserB);
+  gainGateB.connect(soundCtx.destination);
+
+  const detectBeatsB = function () {
     analyserB.getByteTimeDomainData(dataArrayB);
-
     let sum = 0;
-    for (let i = 0; i < bufferLengthB, i++; ) {
+    for (let i = 0; i < bufferLengthB; i++) {
       let normalized = dataArrayB[i] / 128.0 - 1.0;
       sum += normalized * normalized;
     }
@@ -63,37 +73,88 @@ const loadPlayAudioB = async function () {
     if (energy > 0.02) {
       console.log("Beat detected");
     }
-    requestAnimationFrame(detectBeats);
+    requestAnimationFrame(detectBeatsB);
   };
-  detectBeats();
+  detectBeatsB();
 };
 
 let sourceOMW;
+let analyserOMW;
+let gainGateOMW;
 const loadPlayAudioOMW = async function () {
   const file3 = await fetch("On My Way Home.wav");
   const arrayBufferOMW = await file3.arrayBuffer();
-  const audioBufferOMW = await soundCtx.decodeAudioData(arrayBufferOMW);
+  audioBufferOMW = await soundCtx.decodeAudioData(arrayBufferOMW);
   sourceOMW = soundCtx.createBufferSource();
   sourceOMW.buffer = audioBufferOMW;
-  sourceOMW.connect(gainGate);
-  gainGate.connect(soundCtx.destination);
+
+  gainGateOMW = soundCtx.createGain();
+  gainGateOMW.gain.value = 1.0;
+
+  analyserOMW = soundCtx.createAnalyser();
+  analyserOMW.fftSize = 2048;
+  const bufferLengthOMW = analyserOMW.frequencyBinCount;
+  const dataArrayOMW = new Uint8Array(bufferLengthOMW);
+  sourceOMW.connect(gainGateOMW);
+  gainGateOMW.connect(analyserOMW);
+  gainGateOMW.connect(soundCtx.destination);
+
+  const updateAnalyserOMW = function () {
+    analyserOMW.getByteFrequencyData(dataArrayOMW);
+    console.log(dataArrayOMW);
+    requestAnimationFrame(updateAnalyserOMW);
+  };
+  updateAnalyserOMW();
 };
 
 let sourceE;
+let analyserE;
+let gainGateE;
 const loadPlayAudioE = async function () {
   const file3 = await fetch("Everlasting.wav");
   const arrayBufferE = await file3.arrayBuffer();
-  const audioBufferE = await soundCtx.decodeAudioData(arrayBufferE);
+  audioBufferE = await soundCtx.decodeAudioData(arrayBufferE);
   sourceE = soundCtx.createBufferSource();
   sourceE.buffer = audioBufferE;
-  sourceE.connect(gainGate);
-  gainGate.connect(soundCtx.destination);
+
+  gainGateE = soundCtx.createGain();
+  gainGateE.gain.value = 1.0;
+
+  analyserE = soundCtx.createAnalyser();
+  analyserE.fftSize = 512;
+  const bufferLengthE = analyserE.frequencyBinCount;
+  const dataArrayE = new Uint8Array(bufferLengthE);
+
+  sourceE.connect(gainGateE);
+  gainGateE.connect(analyserE);
+  gainGateE.connect(soundCtx.destination);
+
+  const detectBeatsE = function () {
+    analyserE.getByteTimeDomainData(dataArrayE);
+    let sum = 0;
+    for (let i = 0; i < bufferLengthE; i++) {
+      let normalized = dataArrayE[i] / 128.0 - 1.0;
+      sum += normalized * normalized;
+    }
+
+    let energy = sum / bufferLengthE;
+    console.log("Energy:", energy);
+
+    if (energy > 0.02) {
+      console.log("Beat detected");
+    }
+    requestAnimationFrame(detectBeatsE);
+  };
+  detectBeatsE();
 };
 
 let onOffMM = false;
 
 document.addEventListener("DOMContentLoaded", () => {
   loadPlayAudioMM();
+  loadPlayAudioB();
+  loadPlayAudioOMW();
+  loadPlayAudioE();
 });
 
 document
@@ -101,120 +162,144 @@ document
   .addEventListener("click", (event) => {
     if (soundCtx.state == "suspended") {
       soundCtx.resume();
-      sourceMM.start();
     }
 
     let now = soundCtx.currentTime;
 
     if (!onOffMM) {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(1, now + 0.05);
+      sourceMM = soundCtx.createBufferSource();
+      sourceMM.buffer = audioBufferMM;
+      sourceMM.connect(gainGateMM);
+      gainGateMM.connect(soundCtx.destination);
+
+      sourceMM.start();
+
+      gainGateMM.gain.setValueAtTime(gainGateMM.gain.value, now);
+      gainGateMM.gain.linearRampToValueAtTime(1, now + 0.05);
 
       event.target.style.backgroundColor = "aqua";
       event.target.innerText = "stop memory machine";
       onOffMM = true;
     } else {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(0, now + 0.05);
+      gainGateMM.gain.setValueAtTime(gainGateMM.gain.value, now);
+      gainGateMM.gain.linearRampToValueAtTime(0, now + 0.05);
 
       event.target.style.backgroundColor = "pink";
       event.target.innerText = "start memory machine";
       onOffMM = false;
+
+      sourceMM.stop();
+      sourceMM.disconnect();
     }
   });
 
 let onOffB = false;
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadPlayAudioB();
-});
 
 document
   .getElementById("start/stop being")
   .addEventListener("click", (event) => {
     if (soundCtx.state == "suspended") {
       soundCtx.resume();
-      sourceB.start();
     }
     let now = soundCtx.currentTime;
 
     if (!onOffB) {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(1, now + 0.05);
+      sourceB = soundCtx.createBufferSource();
+      sourceB.buffer = audioBufferB;
+      sourceB.connect(gainGateB);
+      gainGateB.connect(soundCtx.destination);
+
+      sourceB.start();
+
+      gainGateB.gain.setValueAtTime(gainGateB.gain.value, now);
+      gainGateB.gain.linearRampToValueAtTime(1, now + 0.05);
 
       event.target.style.backgroundColor = "aqua";
       event.target.innerText = "stop being";
       onOffB = true;
     } else {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(0, now + 0.05);
+      gainGateB.gain.setValueAtTime(gainGateB.gain.value, now);
+      gainGateB.gain.linearRampToValueAtTime(0, now + 0.05);
 
       event.target.style.backgroundColor = "pink";
       event.target.innerText = "start being";
       onOffB = false;
+
+      sourceB.stop();
+      sourceB.disconnect();
     }
   });
 
 let onOffOMW = false;
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadPlayAudioOMW();
-});
 
 document
   .getElementById("start/stop on my way home")
   .addEventListener("click", (event) => {
     if (soundCtx.state == "suspended") {
       soundCtx.resume();
-      sourceOMW.start();
     }
     let now = soundCtx.currentTime;
 
     if (!onOffOMW) {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(1, now + 0.05);
+      sourceOMW = soundCtx.createBufferSource();
+      sourceOMW.buffer = audioBufferOMW;
+      sourceOMW.connect(gainGateOMW);
+      gainGateOMW.connect(soundCtx.destination);
+
+      sourceOMW.start();
+
+      gainGateOMW.gain.setValueAtTime(gainGateOMW.gain.value, now);
+      gainGateOMW.gain.linearRampToValueAtTime(1, now + 0.05);
 
       event.target.style.backgroundColor = "aqua";
       event.target.innerText = "stop on my way home";
       onOffOMW = true;
     } else {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(0, now + 0.05);
+      gainGateOMW.gain.setValueAtTime(gainGateOMW.gain.value, now);
+      gainGateOMW.gain.linearRampToValueAtTime(0, now + 0.05);
 
       event.target.style.backgroundColor = "pink";
       event.target.innerText = "start on my way home";
       onOffOMW = false;
+
+      sourceOMW.stop();
+      sourceOMW.disconnect();
     }
   });
 
 let onOffE = false;
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadPlayAudioE();
-});
 
 document
   .getElementById("start/stop everlasting")
   .addEventListener("click", (event) => {
     if (soundCtx.state == "suspended") {
       soundCtx.resume();
-      sourceE.start();
     }
     let now = soundCtx.currentTime;
 
     if (!onOffE) {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(1, now + 0.05);
+      sourceE = soundCtx.createBufferSource();
+      sourceE.buffer = audioBufferE;
+      sourceE.connect(gainGateE);
+      gainGateE.connect(soundCtx.destination);
+
+      sourceE.start();
+
+      gainGateE.gain.setValueAtTime(gainGateE.gain.value, now);
+      gainGateE.gain.linearRampToValueAtTime(1, now + 0.05);
 
       event.target.style.backgroundColor = "aqua";
       event.target.innerText = "stop everlasting";
       onOffE = true;
     } else {
-      gainGate.gain.setValueAtTime(gainGate.gain.value, now);
-      gainGate.gain.linearRampToValueAtTime(0, now + 0.05);
+      gainGateE.gain.setValueAtTime(gainGateE.gain.value, now);
+      gainGateE.gain.linearRampToValueAtTime(0, now + 0.05);
 
       event.target.style.backgroundColor = "pink";
       event.target.innerText = "start everlasting";
       onOffE = false;
+
+      sourceE.stop();
+      sourceE.disconnect();
     }
   });
--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(700, 600);
 }
 
 function draw() {
